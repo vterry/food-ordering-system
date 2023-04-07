858d0c92073c047dc96bd7a10917c9f3b0f27bbc
vsouza <victor.fsouza2@gmail.com>
2023-04-07 16:05:25 -0300

    [UDEMY COURSE PROGRESS UPDATE] - Class 21: Option 2 - Using Transactional Event Listener from Spring]

---
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/application/OrderCreatedEventApplicationListener.java
@@ -0,0 +1,24 @@
+package com.food.ordering.system.order.service.application;
+
+import com.food.ordering.system.order.service.application.ports.output.repository.message.publisher.payment.OrderCreatedPaymentRequestMessagePublisher;
+import com.food.ordering.system.order.service.domain.event.OrderCreatedEvent;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionalEventListener;
+
+@Slf4j
+@Component
+public class OrderCreatedEventApplicationListener {
+
+    private final OrderCreatedPaymentRequestMessagePublisher orderCreatedPaymentRequestMessagePublisher;
+
+    public OrderCreatedEventApplicationListener(OrderCreatedPaymentRequestMessagePublisher orderCreatedPaymentRequestMessagePublisher) {
+        this.orderCreatedPaymentRequestMessagePublisher = orderCreatedPaymentRequestMessagePublisher;
+    }
+
+    @TransactionalEventListener
+    void process(OrderCreatedEvent orderCreatedEvent){
+        orderCreatedPaymentRequestMessagePublisher.publish(orderCreatedEvent);
+    }
+
+}
